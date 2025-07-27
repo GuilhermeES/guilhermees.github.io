c095fe3f87468bf24d5d5428acfca5215dd83626
Guilherme Estevão <50378131+GuilhermeES@users.noreply.github.com>
2025-07-27 12:02:31 -0300

    Add files via upload

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -11,3 +11,27 @@ overlay.addEventListener('click', () => {
   menu.classList.remove('is-open');
   overlay.classList.remove('blur');
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const video = document.querySelector(".hero-banner__video");
+  const overlay = document.querySelector(".hero-banner__overlay");
+
+  const setFullPageHeight = () => {
+    const totalHeight = Math.max(
+      document.body.scrollHeight,
+      document.documentElement.scrollHeight
+    );
+
+    if (video) {
+      video.style.height = `${totalHeight}px`;
+    }
+
+    if (overlay) {
+      overlay.style.height = `${totalHeight}px`;
+    }
+  };
+
+  setFullPageHeight();
+  window.addEventListener("resize", setFullPageHeight);
+});
